0a7c96f92f7d58b36cc4d3b10848580b15bebe31
anymol <anymol@yandex.ru>
2017-03-22 22:26:43 +0200

    //Configure gradle

--- app/src/main/java/ua/com/getmysite/booksearch/features/MainActivity.java
+++ app/src/main/java/ua/com/getmysite/booksearch/features/MainActivity.java
@@ -1,7 +1,9 @@
-package ua.com.getmysite.booksearch;
+package ua.com.getmysite.booksearch.features;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ua.com.getmysite.booksearch.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/ua/com/getmysite/booksearch/utils/AppUtils.java
@@ -0,0 +1,15 @@
+package ua.com.getmysite.booksearch.utils;
+
+import android.content.Context;
+
+/**
+ * Created by anymo on 19.12.2016.
+ */
+
+public class AppUtils {
+
+    private AppUtils(){
+        throw new UnsupportedOperationException();
+    }
+
+}
